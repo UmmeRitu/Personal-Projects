54e5e94c5cf6ae2b23ff748e734bd734fc3d400e
UmmeRitu <ummeritu@gmail.com>
2024-07-15 13:09:07 +0200

    Travel Diary

---
+++ Travel Blog/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import Navbar from "./components/Navbar"
+import Place from './components/Place';
+import Data from './Data';
+
+// logo icon
+import world from "./images/world.png"
+import pointer from "./images/placeholder.png"
+
+function App() {
+  const dataset = Data.map(myDataFunction)
+  function myDataFunction(elementItems) {
+    return(
+      <Place
+        point = {pointer}
+        details = {elementItems}
+       />
+    );
+  }
+  return (
+    <div className='react--app--container'>
+      <div className="nav--cont--bar">
+        <Navbar img={world} />
+      </div>
+      <div className="app--container">
+        {dataset}
+      </div>
+    </div>
+  );
+}
+
+export default App;
