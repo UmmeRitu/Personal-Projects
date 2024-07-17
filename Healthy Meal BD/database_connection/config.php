a618280919fe560e90b368f64670ce5db3c96812
UmmeRitu <ummeritu@gmail.com>
2024-07-17 13:37:12 +0200

    Healthy Meal BD

---
+++ Healthy Meal BD/database_connection/config.php
@@ -0,0 +1,5 @@
+<?php
+//path = localhost, username =id12113235_meal, password = test123, DBname = id12113235_meal
+
+$conn = mysqli_connect("localhost","id12113235_meal","test123","id12113235_meal") 
+			or die("cannot connect");
